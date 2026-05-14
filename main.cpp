2e47416e31db542343cdff65f2396636980d7872
Alex Stone <stonealex145@gmail.com>
2026-05-14 14:25:53 -0400

    External Library Linkage

--- main.cpp
+++ main.cpp
@@ -1,15 +1,48 @@
+#include <optional>
 #include <print>
 #include <array>
 #include <cstdlib>
 #include <cstdlib>
+#include <span>
+
+#include "wrap.h"
+
+/// What does a writer look like?
+template <typename T>
+concept Writable = requires(T example, std::span<std::byte> bytes) {
+	{ example.write(bytes) } -> std::same_as<size_t>;
+};
+
+
+/// Let's create an example writer object
+struct Stdout {
+	size_t write(std::span<std::byte> bytes) {
+		return 0;
+	}
+};
+
+
+/// Lets take in a writer and do some stuff with it
+void writesomestuff(Writable auto writer) {
+	// Get some bytes and write some stuff
+	std::array<std::byte, 3> arr = {std::byte{3}, std::byte{4}, std::byte{5}};
+	writer.write(arr);
+
+	nothing(); // from wrap h
+}
 
 int main() {
-	std::array<int32_t, 3> arr = {3, 4, 5};
-
+	// Print
 	std::println("Hello, World!");
 	std::println("what does this mean");
 
+	// This is basically a null
+	std::optional<int> something = std::nullopt;
+	something.reset();
 
+	// Examples
+	Stdout output{};
+	writesomestuff(output);
 
 	return EXIT_SUCCESS;
 }
---
+++ wrap.cpp
@@ -0,0 +1,5 @@
+#include "wrap.h"
+
+void nothing() {}
+
+
