425c55c6fd8bcad94e50168139897656c95f88cd
Alex Stone <stonealex145@gmail.com>
2026-05-14 13:54:07 -0400

    Meson Build System

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <print>
+#include <array>
+#include <cstdlib>
+#include <cstdlib>
+
+int main() {
+	std::array<int32_t, 3> arr = {3, 4, 5};
+
+	std::println("Hello, World!");
+	std::println("what does this mean");
+
+
+
+	return EXIT_SUCCESS;
+}
